796022e3cd54a90ac6d94d0877604aa71cd143c9
unknown <daniel.seunghyun.oh@gmail.com>
2017-06-03 23:55:01 +1000

    Edit package.json script for heroku

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ var express = require("express");
 var app = express(); 
 var path = require("path");
 var publicPath = path.join(__dirname, "../public");
+var port = process.env.PORT || 3000;
 console.log(publicPath);
 app.use(express.static(publicPath));
-app.get("/", (req, res)=>{
-    res.sendFile("index.html");
-});
-app.listen(3000, ()=>{
+// app.get("/", (req, res)=>{
+//     res.sendFile("index.html");
+// });
+app.listen(port, ()=>{
     console.log("server connected");
-})
+});
